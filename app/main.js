248d39611e4fe463117e56e2631d431cd0d9afff
Ted Jenkler <tedjenkler@gmail.com>
2025-01-11 00:21:09 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -2,15 +2,25 @@ const readline = require("readline");
 
 const commands = [];
 
+let terminal = true;
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("$ ", (answer) => {
-  const command = answer.split(" ")[0];
-  if (!commands.includes(command)) {
-    console.log(`${command}: command not found`);
-  }
-  rl.close();
-});
+const promptUser = () => {
+  rl.question("$ ", (answer) => {
+    const command = answer.split(" ")[0];
+    if (!commands.includes(command)) {
+      console.log(`${command}: command not found`);
+    }
+    if (terminal) {
+      promptUser();
+    } else {
+      rl.close();
+    }
+  });
+};
+
+promptUser();
