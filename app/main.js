ef57936ec6924b57fd2ceb56d374741800442444
Ted Jenkler <tedjenkler@gmail.com>
2025-01-11 00:36:25 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 const readline = require("readline");
 
-const commands = ["exit", "echo"];
+const commands = ["exit", "echo", "type"];
 
 let terminal = true;
 
@@ -13,14 +13,20 @@ const promptUser = () => {
   rl.question("$ ", (answer) => {
     const promptArray = answer.split(" ");
     const userCommand = promptArray[0];
-    const userInput = promptArray.slice(1);
+    const userInput = promptArray.slice(1).join(" ");
     if (!commands.includes(userCommand)) {
       console.log(`${userCommand}: command not found`);
     } else if (userCommand === "exit") {
       rl.close();
       process.exit(0);
     } else if (userCommand === "echo") {
-      console.log(userInput.join(" "));
+      console.log(userInput);
+    } else if (userCommand === "type") {
+      if (commands.includes(userInput)) {
+        console.log(`${userInput} is a shell builtin`);
+      } else {
+        console.log(`${userInput}: not found`);
+      }
     }
     if (terminal) {
       promptUser();
