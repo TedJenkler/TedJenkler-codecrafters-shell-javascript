1deb3e3c8c2b569fbfd4bd92d4535a4d6be868e3
Ted Jenkler <tedjenkler@gmail.com>
2025-01-10 23:56:44 +0200

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 const readline = require("readline");
 
+const commands = [];
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -8,3 +10,11 @@ const rl = readline.createInterface({
 rl.question("$ ", (answer) => {
   rl.close();
 });
+
+const main = (rl) => {
+  if (commands.includes(rl.question)) {
+    console.log(`invalid_command: ${rl.question.split(" ")[0]} not found`);
+  }
+};
+
+main();
